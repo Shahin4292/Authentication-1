a370526d3ac00494c1f044c35643b08525eae04d
Shahin429 <shahinsarker052@gmail.com>
2025-03-04 15:08:52 +0600

    update

--- lib/res/routes/routes_name.dart
+++ lib/res/routes/routes_name.dart
@@ -2,16 +2,4 @@ class RouteName {
   static const String loginScreen = '/';
 
   static const String signUpScreen = '/sign_up_screen';
-
-  static const String forgetPassword = '/forget_password';
-
-  static const String verificationScreen = '/verification_screen';
-
-  static const String resetYourPasswordScreen = '/resetYourPassword_screen';
-
-  static const String subscriptionScreen = '/subscription_screen';
-
-  static const String movieDashboardScreen = '/movie_dashboard_screen';
-
-  static const String bottomBar = '/bottom_bar';
 }
