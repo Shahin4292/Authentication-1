d8f0195cb2f4ae7ee5c79434403dc2bd767a4210
Shahin429 <shahinsarker052@gmail.com>
2025-03-04 17:08:38 +0600

    first commit

--- lib/view/login_screen/login_screen.dart
+++ lib/view/login_screen/login_screen.dart
@@ -17,78 +17,80 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Form(
-              key: loginController.formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Image.asset(
-                      AppAssets.alpha,
-                      height: 150,
-                      width: 136,
+      body: Center(
+        child: SafeArea(
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Form(
+                key: loginController.formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: Image.asset(
+                        AppAssets.alpha,
+                        height: 150,
+                        width: 136,
+                      ),
                     ),
-                  ),
-                  Text("Login",style: TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 24)),
-                  SizedBox(height: 20),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    spacing: 10,
-                    children: [
-                      ModifiedText(text: "Enter your email", color: Colors.black, size: 15, fontWeight: FontWeight.w400),
-                      CustomTextField(loginController: loginController),
-                      SizedBox(height: 5),
-                      ModifiedText(text: "Enter your password", color: Colors.black, size: 15, fontWeight: FontWeight.w400),
-                      CustomPasswordField(loginController: loginController),
-                      GestureDetector(
-                        // onTap: loginController.forgotPassword,
-                        child: Align(
-                          alignment: Alignment.bottomRight,
-                          child: ModifiedText(
-                            text: 'Forgot Password',
-                            color: AppColor.redShade,
-                            fontWeight: FontWeight.normal,
-                            size: 14,
+                    Text("Login",style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24)),
+                    SizedBox(height: 20),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      spacing: 10,
+                      children: [
+                        ModifiedText(text: "Enter your email", color: Colors.black, size: 15, fontWeight: FontWeight.w400),
+                        CustomTextField(loginController: loginController),
+                        SizedBox(height: 5),
+                        ModifiedText(text: "Enter your password", color: Colors.black, size: 15, fontWeight: FontWeight.w400),
+                        CustomPasswordField(loginController: loginController),
+                        GestureDetector(
+                          // onTap: loginController.forgotPassword,
+                          child: Align(
+                            alignment: Alignment.bottomRight,
+                            child: ModifiedText(
+                              text: 'Forgot Password',
+                              color: AppColor.redShade,
+                              fontWeight: FontWeight.normal,
+                              size: 14,
+                            ),
                           ),
                         ),
-                      ),
-                      RoundButton(
-                        title: 'Login',
-                        onPress: loginController.login,
-                        height: 60,
-                        width: MediaQuery.sizeOf(context).width,
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ModifiedText(
-                          text: "Don't have an account? ",
-                          color: Colors.grey,
-                          size: 14,
-                          fontWeight: FontWeight.normal),
-                      GestureDetector(
-                        // onTap: loginController.navigateToSignUp,
-                        child: ModifiedText(
-                            text: 'Register',
-                            color: Colors.black,
+                        RoundButton(
+                          title: 'Login',
+                          onPress: loginController.login,
+                          height: 60,
+                          width: MediaQuery.sizeOf(context).width,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ModifiedText(
+                            text: "Don't have an account? ",
+                            color: Colors.grey,
                             size: 14,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                ],
+                            fontWeight: FontWeight.normal),
+                        GestureDetector(
+                          // onTap: loginController.navigateToSignUp,
+                          child: ModifiedText(
+                              text: 'Register',
+                              color: Colors.black,
+                              size: 14,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
