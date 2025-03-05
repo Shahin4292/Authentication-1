fe7997a172d4658e7352f2248e6ac6ae0f893d4a
Shahin429 <shahinsarker052@gmail.com>
2025-03-05 15:29:41 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/login': (context) => LoginScreen(),
         '/home': (context) => HomeScreen(),
+
         // '/password-change': (context) => PasswordChangeScreen(),
       },
     );
